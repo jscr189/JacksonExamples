ddba9216123aa65090de1454eb70d8b8a67afd3c
jscr189 <jscr189@gmail.com>
2020-04-15 03:31:34 +0530

    Initial commit

---
+++ src/main/java/com/jackson/demo/JacksonExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.jackson.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacksonExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jackson/demo/JacksonExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackson.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
